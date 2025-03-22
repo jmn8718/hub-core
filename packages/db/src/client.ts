0bf0d8deefecd1e7b6f0d0edd85a8a87f9fa8dc5
Jose Miguel Navarro <jmn8718@gmail.com>
2025-03-22 10:16:56 +0900

    remove libsql implicit import and update versions

--- packages/db/src/client.ts
+++ packages/db/src/client.ts
@@ -1,11 +1,12 @@
-import { createClient } from "@libsql/client";
-import type { Config } from "@libsql/core/api";
 import { drizzle } from "drizzle-orm/libsql";
 import * as schema from "./schemas/index";
 
-export function createDbClient(config: Config) {
-	const client = createClient(config);
-	return drizzle(client, { logger: true, schema });
-}
-
 export type DbClient = ReturnType<typeof drizzle>;
+
+export function createDbClient(config: {
+	url: string;
+	syncUrl?: string;
+	authToken?: string;
+}): DbClient {
+	return drizzle({ connection: config, logger: true, schema });
+}
