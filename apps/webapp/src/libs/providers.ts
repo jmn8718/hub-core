400a51ebd3c27e0972158a61a7f8aecbed9dcf5e
Jose Miguel Navarro <jmn8718@gmail.com>
2025-04-07 11:00:57 +0900

    update version of coros-connect

--- apps/webapp/src/libs/providers.ts
+++ apps/webapp/src/libs/providers.ts
@@ -1,6 +1,4 @@
-import { CorosClient, ProviderManager } from "@repo/clients";
+import { ProviderManager } from "@repo/clients";
 import { db } from "./db.js";
 
 export const manager = new ProviderManager(db);
-const coros = new CorosClient();
-manager.addClient(CorosClient.PROVIDER, coros);
