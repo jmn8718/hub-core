534a98e614bb4bf291efa884fc36b637d504c4a4
Jose Miguel Navarro <jmn8718@gmail.com>
2025-04-21 08:59:51 +0900

    do not apply middleware on admin api webhooks

--- apps/admin/middleware.ts
+++ apps/admin/middleware.ts
@@ -23,10 +23,11 @@ export const config = {
 	matcher: [
 		/*
 		 * Match all request paths except for the ones starting with:
+		 * - api/webhooks (webhooks api)
 		 * - _next/static (static files)
 		 * - _next/image (image optimization files)
 		 * - favicon.ico (favicon file)
 		 */
-		"/((?!_next/static|_next/image|favicon.ico).*)",
+		"/((?!api/webhook|_next/static|_next/image|favicon.ico).*)",
 	],
 };
